c130c314f18c0174fbc17f7192940a4041c13558
Dale <daleban.dev@gmail.com>
2022-05-21 14:52:50 +0800

    chore: import & configure assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sfpro: ["SF Pro Display", "sans-serif"],
+      },
+      colors: {
+        system: {
+          blue: "#007AFF",
+          gray: {
+            1: "#8E8E93",
+            2: "#AEAEB2",
+            5: "#E5E5EA",
+            6: "#F2F2F7",
+            7: "#2C2C2E",
+            chat: "#E9E9EB",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
