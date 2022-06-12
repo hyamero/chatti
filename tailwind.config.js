29ab60f000f761330f24b5a826e62f8db163a6bc
Dale B <daleban.dev@gmail.com>
2022-06-12 21:12:19 +0800

    chore: update tailwind color theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,17 @@ module.exports = {
           gray: {
             1: "#8E8E93",
             2: "#AEAEB2",
+            3: "#C7C7CC",
             5: "#E5E5EA",
             6: "#F2F2F7",
             7: "#2C2C2E",
             chat: "#E9E9EB",
+            chatName: "#3C3C43",
+            dark: {
+              1: "#8E8E93",
+              2: "#636366",
+              3: "#48484A",
+            },
           },
         },
       },
