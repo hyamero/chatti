1b4104a1f87ff582a78b7afee2f69e0f5060d927
Dale B <daleban.dev@gmail.com>
2022-06-18 10:33:39 +0800

    style: add user image beside received chat bubble

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { IUser } from "../../types/model";
 import { useAuth } from "../contexts/AuthContext";
@@ -25,24 +26,36 @@ interface MessageProps {
 }
 
 const Message: React.FC<MessageProps> = ({ message, userData }) => {
+  const sent = message.author.id === userData.id;
+
   return (
-    <>
-      <label
-        className={`${
-          message.author.id === userData.id
-            ? "hidden"
-            : "relative left-3 text-sm text-system-gray-2"
-        }`}
-      >
-        {message.author.name}
-      </label>
-      <p
-        className={`p ${
-          message.author.id === userData.id ? "send" : "receive"
-        }`}
-      >
-        {message.messageValue}
-      </p>
-    </>
+    <div
+      className={`${
+        !sent ? "flex items-end space-x-2 self-start" : "self-end"
+      }`}
+    >
+      <div className={`${sent && "hidden"}`}>
+        <Image
+          width={35}
+          height={35}
+          layout="fixed"
+          src={message.author.photoURL as string}
+          alt="user image"
+          className="z-10 rounded-full"
+        />
+      </div>
+      <div className={`${sent ? "self-end" : "self-start"}`}>
+        <label
+          className={`${
+            sent ? "hidden" : "relative left-3 text-sm text-system-gray-2"
+          }`}
+        >
+          {message.author.name}
+        </label>
+        <p className={`p ${sent ? "send" : "receive"}`}>
+          {message.messageValue}
+        </p>
+      </div>
+    </div>
   );
 };
