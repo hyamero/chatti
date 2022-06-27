dc5d4b07207f0f866084b9f1c7d799284556673c
Dale B <daleban.dev@gmail.com>
2022-06-27 10:50:55 +0800

    style: update landing page

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -7,14 +7,22 @@ export const SignIn: React.FC<SignInProps> = ({}) => {
   const { signIn } = useAuth();
 
   return (
-    <button
-      onClick={() => {
-        signIn();
-      }}
-      type="button"
-      className="login-with-google-btn rounded bg-system-blue p-3 text-white"
-    >
-      Sign in with Google
-    </button>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-[20vw] font-bold leading-none text-white">Chatti</h1>
+      <p className="mb-5 w-[60%] text-center  text-sm text-system-gray-6 lg:w-[70%]  lg:text-xl">
+        Public Chat with Anonymous Mode. Wow look at this landing page, the
+        author was certainly not being lazy. Here a text there a text lorem
+        ipsum here and there
+      </p>
+      <button
+        onClick={() => {
+          signIn();
+        }}
+        type="button"
+        className="login-with-google-btn rounded bg-system-blue p-3 text-white"
+      >
+        Sign in with Google
+      </button>
+    </div>
   );
 };
