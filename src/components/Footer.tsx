7e13b2e3a5b4ca8298df966d8e666f6564acb173
Dale B <daleban.dev@gmail.com>
2022-08-16 15:57:32 +0800

    chore: add footer design

---
+++ src/components/Footer.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+export const Footer = () => {
+  const data = [
+    {
+      title: "Resources",
+      item1: {
+        title: "Help",
+        link: "#",
+      },
+      item2: {
+        title: "Privacy",
+        link: "#",
+      },
+    },
+
+    {
+      title: "Open Source",
+      item1: {
+        title: "Contribute",
+        link: "https://github.com/hyamero/chatti",
+      },
+      item2: {
+        title: "Bug Report",
+        link: "https://github.com/hyamero/chatti/issues",
+      },
+      item3: {
+        title: "Feature Request",
+        link: "https://github.com/hyamero/chatti/issues",
+      },
+    },
+
+    {
+      title: "Contact",
+      item1: {
+        title: "Github",
+        link: "https://github.com/hyamero",
+      },
+      item2: {
+        title: "Twitter",
+        link: "https://twitter.com/dalehyan",
+      },
+      item3: {
+        title: "Instagram",
+        link: "https://www.instagram.com/dale.hyamero/?hl=en",
+      },
+    },
+  ];
+
+  return (
+    <footer className="w-full bg-[#161616] py-14">
+      <div className="mx-auto flex w-[80%] flex-col justify-between gap-10 whitespace-nowrap lg:flex-row lg:gap-40 xl:gap-56">
+        <div className="text-white">
+          <p className="block text-3xl font-medium">Kōkai Chatti.</p>
+          <p className="text-xl font-medium">
+            Made by &nbsp;
+            <a href="https://github.com/hyamero" className="text-system-blue">
+              hyamero
+            </a>
+            .
+          </p>
+        </div>
+
+        <div className="flex w-full justify-between">
+          {data.map((data, i) => {
+            const { title, item1, item2, item3 } = data;
+
+            return (
+              <div key={i} className="space-y-2 text-base">
+                <h3 className="font-medium text-white">{title}</h3>
+                <ul className="flex flex-col space-y-2 font-normal text-system-gray-3 [&>li>a:hover]:text-system-gray-2 [&>li>a:hover]:transition-all">
+                  <li>
+                    <a href={item1.link}>{item1.title}</a>
+                  </li>
+                  <li>
+                    <a href={item2.link}>{item2.title}</a>
+                  </li>
+                  {item3 && (
+                    <li>
+                      <a href={item3.link}>{item3.title}</a>
+                    </li>
+                  )}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,47 +3,51 @@ import Image from "next/image";
 import toast from "react-hot-toast";
 import { useAuth } from "../contexts/AuthContext";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { Footer } from "./Footer";
 
 export const Landing: React.FC = () => {
   const { signIn, user } = useAuth();
 
   return (
-    <div className="mx-auto mt-20 flex h-full w-[80%] flex-col items-center justify-center rounded-t-[3rem] bg-white pt-24">
-      <h1 className="mb-2 text-center text-8xl font-medium text-black">
-        Kōkai Chatti.
-      </h1>
-      <div className="mx-auto w-[60%]">
-        <p className="mb-5 text-center text-2xl font-medium text-black">
-          Getting in touch is more essential than ever. Chat with strangers,
-          people from all over the world. Choose hide your name and photo and
-          chat with anonymity!
-        </p>
-      </div>
+    <>
+      <div className="mx-auto mt-20 mb-10 flex h-full w-[80%] flex-col items-center justify-center rounded-[3rem] bg-white pt-24 pb-16">
+        <h1 className="mb-2 text-center text-8xl font-medium text-black">
+          Kōkai Chatti.
+        </h1>
+        <div className="mx-auto w-[60%]">
+          <p className="mb-5 text-center text-2xl font-medium text-black">
+            Getting in touch is more essential than ever. Chat with strangers,
+            people from all over the world. Choose hide your name and photo and
+            chat with anonymity!
+          </p>
+        </div>
 
-      <button
-        disabled={user ? true : false}
-        type="button"
-        className="mb-12 flex items-center gap-1 text-2xl text-system-blue"
-        onClick={() => {
-          toast.promise(signIn(), {
-            loading: "Sign in...",
-            success: <b>Signed in successfully!</b>,
-            error: <b>Could not sign in.</b>,
-          });
-        }}
-      >
-        <span className="hover-effect"> Sign in with Google</span>
-        <MdOutlineArrowForwardIos className="text-xl" />
-      </button>
-      <div>
-        <Image
-          src="/assets/chatti-sample.png"
-          width={1016}
-          height={720}
-          alt="Chatti Thumbnail"
-          className="rounded-xl"
-        />
+        <button
+          disabled={user ? true : false}
+          type="button"
+          className="mb-12 flex items-center gap-1 text-2xl text-system-blue"
+          onClick={() => {
+            toast.promise(signIn(), {
+              loading: "Sign in...",
+              success: <b>Signed in successfully!</b>,
+              error: <b>Could not sign in.</b>,
+            });
+          }}
+        >
+          <span className="hover-effect"> Sign in with Google</span>
+          <MdOutlineArrowForwardIos className="text-xl" />
+        </button>
+        <div>
+          <Image
+            src="/assets/chatti-sample.png"
+            width={1016}
+            height={720}
+            alt="Chatti Thumbnail"
+            className="rounded-xl"
+          />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
